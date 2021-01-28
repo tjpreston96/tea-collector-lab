fb59f316e86e18a1fb7490f51e75d4220f033a43
tpreston96 <tpreston96@gmail.com>
2021-01-28 14:40:11 -0600

    fix error

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,6 @@
-from django.contrib import admin
-
-# Add the include function to the import
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    # In this case '' represents the root route
-    path("", include("main_app.urls")),
+
 ]
